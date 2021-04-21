f3dc6dae7e8224d61aeead014e31f2890ec23bae
AlexyPawlicki <63868939+AlexyPawlicki@users.noreply.github.com>
2021-04-21 17:17:38 +0200

    Fix mobile header + mobile font-size

--- scripts/header_displacement.js
+++ scripts/header_displacement.js
@@ -15,6 +15,8 @@
 // -------- ------------------- -------- // 
 
 var mq = window.matchMedia("(min-width: 740px)");
+var invertedmq = window.matchMedia("(max-width: 740px)");
+
 
 if (mq.matches) {
     $(window).on('scroll', function(){
@@ -29,3 +31,21 @@ if (mq.matches) {
     })
 }
 
+
+
+
+
+if (invertedmq.matches) {    
+    $(window).on('scroll', function(){
+        if($(window).scrollTop()){
+            $("header").css("background-size", "100% 100%");
+            $("header").css("height", "5rem");
+            $(".header_logo").css("top", "2px");
+        } 
+        else{
+            $("header").css("background-size", "100% 0%");
+            $("header").css("height", "5.5rem");
+            $(".header_logo").css("top", "5px");
+        }
+    })
+}
