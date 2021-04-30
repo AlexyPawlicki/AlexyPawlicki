2406e5be639c151e225f7f4258501798f6b21249
AlexyPawlicki <63868939+AlexyPawlicki@users.noreply.github.com>
2021-04-30 19:09:11 +0200

    add fourth project + small edits on projects description
    -add 10+ project images

--- scripts/work_content.js
+++ scripts/work_content.js
@@ -7,7 +7,7 @@ $("#card_1-img").css( "background-image", "url(./assets/work_assets/project/1/1.
 $("#card_2-img").css( "background-image", "url(./assets/work_assets/project/2/0.png)");
 $("#card_3-img").css( "background-image", "url(./assets/work_assets/project/3/1_2.png)");
 $("#card_4-img").css( "background-image", "url(https://images.unsplash.com/photo-1619314428206-e475f6648ccf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1396&q=80)");
-$("#card_5-img").css( "background-image", "url(https://images.unsplash.com/photo-1619495447576-a3a86bb4cf0b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80)");
+$("#card_5-img").css( "background-image", "url(./assets/work_assets/project/5/0.png)");
 $("#card_6-img").css( "background-image", "url(https://images.unsplash.com/photo-1619508422331-606e38f1f902?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80)");
 
 
@@ -76,9 +76,9 @@ function card2() {
 
     // texts
     func_project_link("https://github.com/AlexyPawlicki/AlexyPawlicki.github.io", "Voir sur GitHub");
-    create_tags("HTML");
-    create_tags("CSS");
-    create_tags("JS");
+    create_tags("HTML/CSS/JS");
+    create_tags("Photoshop");
+    create_tags("Affinity Designer");
     title_modifier("Mon portfolio");
     paragraph_modifier(`
     Après des dizaines d'heures de réflexion et de maquettage, vous naviguez actuellement sur ce qui est la version 1.0 de mon portfolio ! 
@@ -124,13 +124,13 @@ function card3() {
     // texts
     func_project_link("https://replit.com/@AlexyPawlicki/Carte-de-Jeu-de-Societe?v=1", "Voir sur Replit");
     create_tags("HTML/CSS/JS");
-    create_tags("Angular");
     create_tags("Photoshop");
+    create_tags("Angular");
     title_modifier("Projet - 2021");
     paragraph_modifier(`
-    Pour ce projet tutoret, il nous a été incombé de créer de A à Z une plateforme d'achat de jeux de sociétés, et ce, en 18h.
+    Pour ce dernier projet tutoret, la consigne demeura la même qu'au précèdent : créer un site d'achat de jeux de société, en groupe, et en un peu moins de deux journée de travail.  
     <br> <br>
-    J'ai ainsi conceptualisé l'identité de notre entreprise fictive et j'ai activement participé à l'élaboration du squelette de la page (réalisation des cartes, images, ...).
+    J'ai ainsi conceptualisé l'identité de notre entreprise fictive et j'ai activement participé à l'élaboration de la page (réalisation des cartes, images, ...).
     `);
 
     // images
@@ -211,10 +211,16 @@ function card5() {
     clear_tags();
 
     // texts
-    create_tags("Software");
-    create_tags("Software");
-    title_modifier("Titre du projet 5");
-    paragraph_modifier('Description du projet');
+    func_project_link("https://github.com/MarieVRT/ProjetS3-Marathon-ludotheque", "Voir sur GitHub");
+    create_tags("HTML/CSS");
+    create_tags("Photoshop");
+    create_tags("Laravel");
+    title_modifier("Marathon 2020");
+    paragraph_modifier(`
+    Dans le cadre du projet tutoret de troisième semestre, il nous a été incombé de créer de A à Z une plateforme d'achat de jeux de sociétés par groupe, et ce, en 18h.
+    <br> <br>
+    Cela m'a amené à produire une <a target="_blank" href="./assets/work_assets/project/5/charte.pdf" class="project_pdf">charte graphique</a>, un logo et un squelette pour ce site fictif.
+    `);
 
     // images
     var bg = $("#card_5-img").css("background-image");
@@ -225,13 +231,13 @@ function card5() {
         img_switch(1, bg.toString());
     });
     $("#pre_2").click(function(){
-        img_switch(2, "https://images.unsplash.com/photo-1619616710561-1d0ab63f2272?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80");
+        img_switch(2, "./assets/work_assets/project/5/2_3.png");
     });
     $("#pre_3").click(function(){
-        img_switch(3, "https://images.unsplash.com/photo-1610593469791-209a5ab288bb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=640&q=80");
+        img_switch(3, "./assets/work_assets/project/5/3_3.png");
     });
     $("#pre_4").click(function(){
-        img_switch(4, "./assets/work_assets/project/1/2.svg");
+        img_switch(4, "./assets/work_assets/project/5/5_3.png");
     });
     
 
